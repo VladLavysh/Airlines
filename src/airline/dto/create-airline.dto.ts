45e78caf0e9932fa283da491e24192059af89cbf
Vladislav <miegeff@gmail.com>
2026-02-27 09:44:02 +0100

    create airline DTO validation fix

--- src/airline/dto/create-airline.dto.ts
+++ src/airline/dto/create-airline.dto.ts
@@ -1,16 +1,18 @@
-import { IsString, MaxLength } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 import { IAirline } from '../types/airline.interface';
 
 export class CreateAirlineDto implements IAirline {
+  @IsNotEmpty()
   @IsString()
   @MaxLength(64)
   name: string;
 
-  @IsString()
+  @IsNotEmpty()
   @IsString()
   @MaxLength(3)
   iata_code: string;
 
+  @IsNotEmpty()
   @IsString()
   @MaxLength(32)
   country: string;
