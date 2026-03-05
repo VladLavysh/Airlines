a0153db36b0d1553addd9102597946687d47b86e
Vladislav <miegeff@gmail.com>
2026-03-05 16:45:27 +0100

    flight schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, varchar, integer } from 'drizzle-orm/pg-core';
+import { pgTable, serial, varchar, integer, timestamp } from 'drizzle-orm/pg-core';
 import { index } from 'drizzle-orm/pg-core';
 
 export const airline = pgTable('airline', {
@@ -36,4 +36,38 @@ export const route = pgTable(
     arrival_airport: varchar('arrival_airport', { length: 32 }).notNull(),
     distanceKM: integer('distance').notNull()
   }
+)
+
+export const flight = pgTable(
+  'flight',
+  {
+    id: serial('id').primaryKey(),
+    departure_time: timestamp('departure_time').notNull(),
+    arrival_time: timestamp('arrival_time').notNull(),
+    status: varchar('arrival_airport', { length: 32 }).notNull(),
+
+    route_id: integer('route_id')
+      .notNull()
+      .references(() => airline.id, {
+        onDelete: 'restrict',
+        onUpdate: 'cascade'
+      }),
+    aircraft_id: integer('aircraft_id')
+      .notNull()
+      .references(() => airline.id, {
+        onDelete: 'restrict',
+        onUpdate: 'cascade'
+      }),
+    airline_id: integer('airline_id')
+      .notNull()
+      .references(() => airline.id, {
+        onDelete: 'restrict',
+        onUpdate: 'cascade'
+      })
+  },
+  (table) => [
+    index('flight_route_id_idx').on(table.route_id),
+    index('flight_aircraft_id_idx').on(table.aircraft_id),
+    index('flight_airline_id_idx').on(table.airline_id)
+  ]
 )
